68a661e62f26c4be2b1e1b75f72353cdec14da8b
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-01-09 13:02:18 +0530

    Implement disc_usage table creation and data insertion
    Added functionality to create a disc_usage table and insert data.

--- sqlite3/usage.py
+++ sqlite3/usage.py
@@ -8,6 +8,16 @@ cursor = conn.cursor()
 cursor.execute("CREATE TABLE IF NOT EXISTS uptime (server_ip TEXT, percent INTEGER)")
 cursor.execute("INSERT INTO uptime VALUES ('10.0.0.1', 99)")
 
+# Inserting items into DB
+cursor.execute("CREATE TABLE IF NOT EXISTS disc_usage (date TEXT, percent INTEGER)")
+cursor.execute(f"INSERT INTO disc_usage VALUES (?, ?)", (datetime.datetime.now().isoformat(), disc_usage))
+
+# Fetching all data
+cursor.execute("SELECT * FROM disc_usage")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
 # 3. Save & Close
 conn.commit()
 conn.close()
