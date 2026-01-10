c29e1a5b5887bfd9f382484b6180ef98ecbdc664
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-01-10 16:47:24 +0530

    Add threading example with worker function

---
+++ threading/syntax.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+def worker(name):
+    print(f"Worker {name} started")
+    time.sleep(2)
+    print(f"Worker {name} started")
+    
+
+# Create Threads 
+# target = the function to run # args = the arguments to pass to that function (must be a tuple)
+t1 = threading.Thread(target=worker, args=("A",))
+t2 = threading.Thread(target=worker, args=("B",))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
