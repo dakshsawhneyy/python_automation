ed80fbac3a3eb1819875711a04d384809a1b4235
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-01-10 16:59:39 +0530

    Update website_status_checker.py

--- threading/website_status_checker.py
+++ threading/website_status_checker.py
@@ -1,4 +1,5 @@
 urls = ["https://google.com", "https://github.com", "https://amazon.com"]
+threads = []    # we will fire all simultaneously and then join them 
 
 def check_status(url):
     result = subprocess.run(["curl", "-s", "--max-time", "5", url], text=True, capture_output=True)
@@ -9,5 +10,9 @@ def check_status(url):
 for url in urls:
     t = threading.Thread(target=check_status, args=(url,))
     
-    t.start()
+    t.start()   # fire everyone consecutively 
+    threads.append(t)
+    
+# Loop over threads to join them
+for t in threads:
     t.join()
