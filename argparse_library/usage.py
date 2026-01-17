7800b6006a9d4d567b2447ebbd5ad52884252a72
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2026-01-17 10:38:45 +0530

    Update usage.py

--- argparse_library/usage.py
+++ argparse_library/usage.py
@@ -3,7 +3,10 @@ import argparse
 parser = argparse.ArgumentParser(description="EC2 Manager")
 parser.add_argument("--instance-id", type=str, required=True)
 parser.add_argument("--region", type=str, default="us-east-1", required=False)
+parser.add_argument("--dry-run", required=False, action="store_true")
 
 args = parser.parse_args()
+
+print(args.dry_run)  # Either True or False
 
 print(f"Stopping instance {args.instance_id} in region {args.region}...")
